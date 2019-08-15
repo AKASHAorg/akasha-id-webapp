b6edfbdbac2487219e7818e456500d222256df9e
Andrei Sambra <andrei.sambra@akasha.world>
2019-08-15 10:29:35 +0300

    added login/logout functionality

--- test/test-app/app.js
+++ test/test-app/app.js
@@ -11,24 +11,52 @@ const Masq = new masq.Masq(
     }
 )
 
+const hideProfile = () => {
+    console.log('hiding profile')
+    document.getElementById('pic').src = ''
+    document.getElementById('did').innerText = ''
+}
+
 const displayProfile = async () => {
+    hideLogin()
     document.getElementById('pic').src = await Masq.getProfileImage()
     document.getElementById('did').innerText = await Masq.getDID()
+    const logout = document.createElement('button')
+    logout.innerText = 'Logout'
+    logout.id = 'logout'
+    logout.addEventListener('click', async () => {
+        await Masq._resetLogin()
+        hideProfile()
+        showLogin()
+    }, false)
+    document.getElementsByTagName('body')[0].appendChild(logout)
+}
+
+const showLogin = async () => {
+    const link = await Masq.getLoginLink()
+    console.log(link)
+    const a = document.getElementById('link')
+    a.href = link
+    a.innerText = 'Login / register'
+    document.getElementById('qr').src = 'https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=' + encodeURIComponent(link)
+    document.getElementById('logout').remove()
+    // Display link to the user
+}
+
+const hideLogin = () => {
+    const a = document.getElementById('link')
+    a.innerText = ''
+    a.href = ''
+    document.getElementById('qr').src = ''
 }
 
 init = async () => {
     await Masq.init()
     if (Masq.isLoggedIn()) {
-        await Masq.logIntoMasq()
         status.innerText = Masq.state
+        await displayProfile()
     } else {
-        const link = await Masq.getLoginLink()
-        console.log(link)
-        const a = document.getElementById('link')
-        a.href = link
-        a.innerText = link
-        document.getElementById('qr').src = 'https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=' + encodeURIComponent(link)
-        // Display link to the user
+        showLogin()
         // and ask if he wants to stay connected on restart until he signs out
         const stayConnected = true
         // Once the link is opened, execute the following code
